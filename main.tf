1eeadfc5d2cfe72abd5edfd0e06e1f17d815ff07
ajith <ajith@gmail.com>
2025-05-15 13:38:28 +0530

    Initial commit

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx_container" {
   name  = "nginx-server"
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.name
   ports {
     internal = 80
     external = 8080
