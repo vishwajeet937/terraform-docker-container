9aa32f8cccdfd95d1bd8b148f68b4d7d4bd3889c
ajith <ajith@gmail.com>
2025-05-15 13:13:09 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx-server"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
